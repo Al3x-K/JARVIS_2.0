3df44e9969d41d21f73680f96da0fd991ce05310
Al3x-K <ola.kail1901@gmail.com>
2024-02-20 22:39:43 +0000

    added functions to get current time and current date

--- J.A.R.V.I.S_0.1.py
+++ J.A.R.V.I.S_0.1.py
@@ -1,5 +1,5 @@
 import pyttsx3 # pip install pyttsx3 == text data to speech
-
+import datetime # pip install datetime == date and time
 engine = pyttsx3.init() # initialize the engine
 
 
@@ -8,7 +8,7 @@ def speak(audio):
     engine.say(audio) # text to speechs
     engine.runAndWait() # run and wait for the command
 
-def getVoices(voice):
+def chooseVoice(voice):
     voices = engine.getProperty('voices') # get the voices
     print(voices[1].id) # print the voices
     if voice == 1:
@@ -18,7 +18,24 @@ def getVoices(voice):
 
     speak ("hello, how can i help you") # call the function and pass the text to it
 
+def getCurrentTime():
+    Time = datetime.datetime.now().strftime("%I:%M:%S") # get the time
+    speak("The current time is: ") 
+    speak(Time)
+
+def getCurrentDate():
+    year = datetime.datetime.now().year # get the year
+    month = datetime.datetime.now().month # get the month
+    day = datetime.datetime.now().day # get the day
+    speak("The current date is: ")
+    speak(day)
+    speak(month)
+    speak(year)
+
 while True:
     voice = int(input("Press 1 for male voice\nPress 2 for female voice\n")) # input the text
     #speak(audio) # call the function and pass the text to it
-    getVoices(voice) # call the function
+    chooseVoice(voice) # call the function
+    getCurrentTime()
+    getCurrentDate()
+    
