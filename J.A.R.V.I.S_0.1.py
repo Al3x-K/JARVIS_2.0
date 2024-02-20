afa2a57c00f4c12ea6bc5d77e810293a2118257e
Al3x-K <ola.kail1901@gmail.com>
2024-02-20 21:25:29 +0000

    text to speech basics

---
+++ J.A.R.V.I.S_0.1.py
@@ -0,0 +1,6 @@
+import pyttsx3 # pip install pyttsx3 == text data to speech
+
+engine = pyttsx3.init() # initialize the engine
+
+engine.say("Hello, Ate here.") # text to speechs
+engine.runAndWait() # run and wait for the command
