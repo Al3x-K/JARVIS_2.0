2be44740c31a3b074ecb2f0a73c79d6216faaf39
Al3x-K <ola.kail1901@gmail.com>
2024-02-20 21:38:12 +0000

    text to speech function + user input

--- J.A.R.V.I.S_0.1.py
+++ J.A.R.V.I.S_0.1.py
@@ -2,5 +2,13 @@ import pyttsx3 # pip install pyttsx3 == text data to speech
 
 engine = pyttsx3.init() # initialize the engine
 
-engine.say("Hello, Ate here.") # text to speechs
-engine.runAndWait() # run and wait for the command
+
+# speak function
+def speak(audio):
+    engine.say(audio) # text to speechs
+    engine.runAndWait() # run and wait for the command
+
+while True:
+    audio = input("Enter the text to convert it into speach: \n") # input the text
+    speak(audio) # call the function and pass the text to it
+  
