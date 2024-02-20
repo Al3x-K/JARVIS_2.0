2a91ac7dc6664a9a017e3084cc91ff088532ce8f
Al3x-K <ola.kail1901@gmail.com>
2024-02-20 22:22:11 +0000

    choosing the voice function
    the function allows the user to choose either a male or female voice for the assistant

--- J.A.R.V.I.S_0.1.py
+++ J.A.R.V.I.S_0.1.py
@@ -8,7 +8,17 @@ def speak(audio):
     engine.say(audio) # text to speechs
     engine.runAndWait() # run and wait for the command
 
+def getVoices(voice):
+    voices = engine.getProperty('voices') # get the voices
+    print(voices[1].id) # print the voices
+    if voice == 1:
+        engine.setProperty('voice', voices[1].id) # set the male voice
+    if voice == 2:
+        engine.setProperty('voice', voices[-2].id) # set the female voice
+
+    speak ("hello, how can i help you") # call the function and pass the text to it
+
 while True:
-    audio = input("Enter the text to convert it into speach: \n") # input the text
-    speak(audio) # call the function and pass the text to it
-  
+    voice = int(input("Press 1 for male voice\nPress 2 for female voice\n")) # input the text
+    #speak(audio) # call the function and pass the text to it
+    getVoices(voice) # call the function
